22e7b4393d83e5344acf7705a77604510830d570
James Tan <james_tan97@outlook.com>
2023-04-10 00:43:11 -0400

    Initial commit - Add split script

---
+++ README.md
@@ -0,0 +1,8 @@
+# pgn_processor
+
+## Split
+Split up a PGN file that contains multiple games.
+
+```bash
+python split.py -i "input.pgn" -o "output_dir"
+```
---
+++ split.py
@@ -0,0 +1,51 @@
+import argparse
+from pathlib import Path
+from tqdm import tqdm
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input_pgn", type=Path, required=True)
+    parser.add_argument("-o", "--output_dir", type=Path, required=True)
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    if not args.output_dir.exists():
+        args.output_dir.mkdir(parents=True)
+
+    process_pgn_file(args.input_pgn, args.output_dir)
+
+
+def process_pgn_file(filename, output_dir: Path):
+    with open(filename, "r") as f:
+        curr_game = []
+        for line in tqdm(f):
+            if "[Event" in line:
+                if len(curr_game) != 0:
+                    save_game(curr_game, output_dir)
+                curr_game = [line]
+            else:
+                curr_game.append(line)
+        # Handle last game
+        if len(curr_game) != 0:
+            save_game(curr_game, output_dir)
+
+
+def save_game(game: list, output_dir):
+    white = next(filter(lambda x: x.startswith("[White"), game)).split('"')[1]
+    black = next(filter(lambda x: x.startswith("[Black"), game)).split('"')[1]
+    output_name = f"{white} - {black}.pgn"
+    game_txt = "".join(game)
+
+    save_path = output_dir / output_name
+    with open(save_path, "w") as fout:
+        fout.write(game_txt)
+
+
+if __name__ == "__main__":
+    main()
