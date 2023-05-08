cc1b3e270f52d61e502080f78d4c178aec7d8d5b
James Tan <james_tan97@outlook.com>
2023-05-08 03:49:45 -0400

    Add another convenience script

---
+++ split-and-merge.sh
@@ -0,0 +1,14 @@
+set -e
+
+# path to individual pgn
+PGN=$1
+OUT_DIR=$2
+
+mkdir -p "$OUT_DIR/individuals" "$OUT_DIR/merged"
+
+python split.py -i $PGN -o "$OUT_DIR/individuals"
+
+python get-uniques.py -i "$OUT_DIR/individuals" | while read -r name; do
+    echo "Merging $name"
+    python merge-pgn.py -i "$OUT_DIR/individuals/$name*" -o "$OUT_DIR/merged/$name.pgn"
+done
